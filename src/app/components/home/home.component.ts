ff7ab7c572b06e07f5b716e1659ebbb496dfce6d
Manuel <zambra4a@gmail.com>
2017-05-14 18:24:23 +0200

    Redisigned

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,63 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  skills:any[] = ['html5','java','css3','javascript','bootstrap','angular','liferay','unity','jquery'];
-  positions:any[] = [];
-  px:string = 'px';
-
-  constructor() {
-
-  }
-
-  ngOnInit() {
-
-    for(let p of this.skills) {
-
-      this.positions.push({
-        'top' : Math.floor(Math.random() * window.innerHeight * 0.99),
-        'left' : Math.floor(Math.random() * window.innerWidth)
-      });
-    }
-
-    this.animate();
-
-  }
-
-  animate(){
-
-
-    for(var i = 0; i< this.positions.length; i++) {
-
-      if(this.positions[i].top > window.innerHeight - 100) {
-
-        this.positions[i].top = Math.floor(Math.random() * window.innerHeight - 100);
-
-      } else {
-
-        this.positions[i].top += Math.floor(Math.random() + 1) + 5;
-
-      }
-      if(this.positions[i].left > window.innerWidth - 150) {
-
-        this.positions[i].left = 0;
-
-      } else {
-
-        this.positions[i].left += Math.floor(Math.random()) + 1;
-
-      }
-
-    }
-
-    requestAnimationFrame(()=>this.animate());
-  }
-
+  constructor() {}
 
   openLinkedin(){
     let url = "https://www.linkedin.com/in/manuel-zambrano-4507b948/";
